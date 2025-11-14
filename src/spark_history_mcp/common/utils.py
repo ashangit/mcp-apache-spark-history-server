6cfa1fa027945a92fcb86271e23901852987dd4a
Benjamin Kermani <benjamin.kermani@datadoghq.com>
2025-11-14 11:35:41 +0100

    add workflow URL to yoshi jobs

--- src/spark_history_mcp/common/utils.py
+++ src/spark_history_mcp/common/utils.py
@@ -3,6 +3,3 @@ from ..common.variable import DD_DATACENTER
 
 def get_spark_history_server_url_for_user(app_id: str, datacenter:str = DD_DATACENTER):
     return f"https://spark-history-server.{datacenter}/history/{app_id}/jobs/"
-
-def get_atlas_workflow_url_for_user(workflow_id: str, datacenter:str = DD_DATACENTER):
-    return f"https://temporal.{datacenter}/namespaces/v1:dataandanalytics-0/workflows/{workflow_id}"
